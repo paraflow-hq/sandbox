fcbb2f32af72c0c2139dbda5bc49bfa22dc3fb3b
wuhuajia <wuhuajia@kanyun.com>
2025-12-10 12:21:17 +0800

    feat: 指定安装 claude-agent-sdk==0.1.6  版本

--- template.py
+++ template.py
@@ -16,7 +16,7 @@ template = (
     })
     .run_cmd("mkdir -p $PNPM_HOME")
     .run_cmd("curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg && echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main\" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null && apt-get update && apt-get install -y gh")
-    .run_cmd("pip install claude-code-sdk claude-agent-sdk anyio boto3 e2b-code-interpreter GitPython datadog-api-client")
+    .run_cmd("pip install claude-code-sdk claude-agent-sdk==0.1.6 anyio boto3 e2b-code-interpreter GitPython datadog-api-client")
     .set_user("user")
     .set_workdir("/home/user")
 )
