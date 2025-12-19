d5ade72de987c50c493ef50ef41422c0a7617857
dongrubing <dongrubing@paraflow.com>
2025-12-19 16:37:10 +0800

    fix: 修改agent版本

--- template.py
+++ template.py
@@ -6,7 +6,7 @@ template = (
     .set_user("root")
     .set_workdir("/")
     .run_cmd("sysctl fs.inotify.max_user_watches=131070 && sysctl -p")
-    .run_cmd("npm install -g tsx@^4.7.0 @anthropic-ai/claude-code@2.0.36 @anthropic-ai/claude-agent-sdk@^0.1.6 axios@^1.6.0 adm-zip@^0.5.10 form-data@^4.0.0 @datadog/datadog-api-client tar")
+    .run_cmd("npm install -g tsx@^4.7.0 @anthropic-ai/claude-agent-sdk@^0.1.36 axios@^1.6.0 adm-zip@^0.5.10 form-data@^4.0.0 @datadog/datadog-api-client tar")
     .run_cmd("npx playwright install chrome")
     .run_cmd("npm install -g pnpm")
     .set_envs({
