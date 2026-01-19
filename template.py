0de10d215edf4bd30246d82932d6d20a03c4a383
BoWen Chai <chaibowen@kanyun.com>
2026-01-19 19:29:04 +0800

    fixup: change order

--- template.py
+++ template.py
@@ -16,7 +16,6 @@ template = (
         "PATH": "$PNPM_HOME:$PATH",
     })
     .run_cmd("mkdir -p $PNPM_HOME")
-    .run_cmd("pnpm install -g @ast-grep/cli")
     .run_cmd("curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg && echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main\" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null && apt-get update && apt-get install -y gh")
     .run_cmd("pip install claude-code-sdk claude-agent-sdk==0.1.6 anyio boto3 e2b-code-interpreter GitPython datadog-api-client")
     .set_user("user")
@@ -26,4 +25,5 @@ template = (
     .run_cmd("unzip /home/user/txom-prompt.main.zip -d /home/user/txom && cd /home/user/txom/txom-dev/demo/ && pnpm install")
     .run_cmd("rm -rf /home/user/txom-prompt.main.zip")
     .run_cmd("rm -rf /home/user/txom")
+    .run_cmd("pnpm install -g @ast-grep/cli")
 )
