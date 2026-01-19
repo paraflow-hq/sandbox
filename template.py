fa3c92a947eb59d13a26a8684fde8a76d8a69069
BoWen Chai <chaibowen@kanyun.com>
2026-01-19 19:22:26 +0800

    feat: make ast grep global avaliable

--- template.py
+++ template.py
@@ -9,7 +9,6 @@ template = (
     .run_cmd("npm install -g tsx@^4.7.0 @anthropic-ai/claude-code@2.0.36 axios@^1.6.0 adm-zip@^0.5.10 form-data@^4.0.0 @datadog/datadog-api-client tar systeminformation")
     .run_cmd("npx playwright install chrome")
     .run_cmd("npm install -g pnpm")
-    .run_cmd("pnpm add -g @ast-grep/cli")
     .set_envs({
         "PNPM_HOME": "/root/.local/share/pnpm",
     })
@@ -17,6 +16,7 @@ template = (
         "PATH": "$PNPM_HOME:$PATH",
     })
     .run_cmd("mkdir -p $PNPM_HOME")
+    .run_cmd("pnpm add -g @ast-grep/cli")
     .run_cmd("curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg && echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main\" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null && apt-get update && apt-get install -y gh")
     .run_cmd("pip install claude-code-sdk claude-agent-sdk==0.1.6 anyio boto3 e2b-code-interpreter GitPython datadog-api-client")
     .set_user("user")
