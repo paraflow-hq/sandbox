91eb030f436f0ddc8d071bfb60c89902afb90b72
daniel <daniel.lin@paraflow.com>
2026-04-03 08:19:43 +0000

    Add iptables to pre-installed system packages
    Co-Authored-By: daniel via Moxt <noreply@moxt.ai>

--- template.py
+++ template.py
@@ -9,7 +9,7 @@ template = (
     .run_cmd("sysctl fs.inotify.max_user_watches=131070 && sysctl -p")
     # 安装系统工具
     .run_cmd("apt-get update")
-    .run_cmd("apt-get install -y strace htop curl wget unzip")
+    .run_cmd("apt-get install -y strace htop curl wget unzip iptables")
     # 安装 GitHub CLI
     .run_cmd(
         'curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null'
